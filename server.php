44210241a38f59e22b220ecd5ad060115e5514f7
oosvaldo <oosvaldo.leoon@gmail.com>
2015-02-28 02:40:16 -0800

    If the word isn't in Spanish table seach it in the English one

--- server.php
+++ server.php
@@ -23,8 +23,15 @@ function getMeaning($word ) {
 				JOIN t_eword_sword AS es ON s.id = es.sword 
 				JOIN t_ewords AS e ON es.eword = e.id 
 				WHERE e.word = '$word'";
+	$result = Mysqls::run($query);
 
-	$result 	= Mysqls::run($query);
+	if(!$result) {
+		$query 	= "SELECT e.word FROM t_ewords AS e 
+					JOIN t_eword_sword AS es ON e.id = es.eword 
+					JOIN t_swords AS s ON es.eword = s.id 
+					WHERE s.word = '$word'";
+		$result = Mysqls::run($query);
+	}
 
 	echo "$word : ". $result['word'];
 }
