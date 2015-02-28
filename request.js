b64742c28cf541b67332e141b1c7daf94d4b734a
oosvaldo <oosvaldo.leoon@gmail.com>
2015-02-28 06:00:08 -0800

    Implement add new words request ajax

--- request.js
+++ request.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
+	if($("#meaning").val() === ''){
+		$('#t_save').hide();
+	}
+	else{
+		$('#t_send').hide();
+	}
 
+	$("#meaning").blur(function(){
+		if($(this).val() === ''){
+			$('#t_save').hide();
+			$('#t_send').show();
+		}
+		console.log($(this).val());
+	});
+
+	$("#meaning").focus(function(){
+		$('#t_save').show();
+		$('#t_send').hide();
+	});
 });
-
 
 function sendWord(){
 	var meaning = $("#meaning"),
@@ -12,16 +29,17 @@ function sendWord(){
 			data: {'service': 'get', 'word': word},
 			type: "POST",
 			url : "server.php",
-			success: function(response){
-				if(response.indexOf('200') > 0){
-					if(response.indexOf('en') > 0){
+			success: function(res){
+				if(res.indexOf('200') > 0){
+					if(res.indexOf('en') > 0){
 						$("#langmw").html('English');
 						$("#langme").html('Spanish');
 					} else {
 						$("#langme").html('English');
 						$("#langmw").html('Spanish')
 					}
-					meaning.val(response);
+					meaning.val(res);
+					//meaning.addAttr('disabled');
 				} else {
 					meaning.val('NO FOUND');
 					meaning.removeClass('word_box');
@@ -30,10 +48,26 @@ function sendWord(){
 						meaning.removeClass('nofound');
 						meaning.addClass('word_box');
 						meaning.val('');
-						meaning.removeAttr('disabled');
 					}, 2000);
 				}
 			} 
+		});
+	}
+}
+
+// Send the meaning and the word to save
+function saveWord(){
+	var meaning = $("#meaning"),
+	 	word 	= $("#myword").val();
+
+	if(word !== '' && meaning.val() !== ''){
+		$.ajax({
+			data: {'service': 'set', 'word': word, 'mean' : meaning.val()},
+			type: "POST",
+			url : "server.php",
+			success: function(res){
+				meaning.val(res);
+			}
 		});
 	}
 }
